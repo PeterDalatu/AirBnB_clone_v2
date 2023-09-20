7c16b7cc10540604d3780141009806c7c6ed171b
PeterDalatu <gurustech01@gmail.com>
2023-09-20 16:52:53 -0700

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,21 @@
-#!/usr/bin/python3
-"""Amenity subclass that inherits from BaseModel"""
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class that represents new amenities"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
